2ab3e87c2393187ebf93e4e2e948e220e0b7c179
apolloniiaa <apoll93@hotmail.com>
2023-08-23 21:12:25 +0200

    dropdown functionality

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const arrows = document.querySelectorAll('.collapsible-arrow');
-arrows.forEach((arrow) => {
-  arrow.addEventListener('click', () => {
-    const row = arrow.closest('.data-row');
-    row.classList.toggle('hide');
+document.addEventListener('DOMContentLoaded', function () {
+  const toggleIcons = document.querySelectorAll('.toggle-icon');
 
-    // Állítsd be a max-height értékét a sor megjelenítéséhez
-    if (!row.classList.contains('hide')) {
-      row.style.maxHeight = row.scrollHeight + 'px';
-    } else {
-      row.style.maxHeight = null;
-    }
+  toggleIcons.forEach((icon) => {
+    icon.addEventListener('click', function () {
+      const row = this.closest('.toggle-icon');
+      const rowsToShow = row.parentNode.querySelectorAll('.show-more');
+      rowsToShow.forEach((row) => {
+        row.style.display = row.style.display === 'none' ? 'table-row' : 'none';
+      });
+    });
   });
 });
